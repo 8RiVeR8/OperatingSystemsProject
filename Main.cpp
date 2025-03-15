09bcf45a81a49920eed9f0fa8300426a06833a13
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-03-15 22:18:54 +0100

    add CMakeList

--- Main.cpp
+++ Main.cpp
@@ -4,38 +4,56 @@
 #include <thread>
 
 pthread_mutex_t* waitForFork;
+int amountOfPhilosophers;
 
 void *philosopher(void *arg) {
     int id = *reinterpret_cast<int*>(arg);
-    std::cout << "Filozof numer: " << id << " myśli\n";
+    int firstFork = std::min(id, (id + 1) % amountOfPhilosophers);
+    int secondFork = std::max(id, (id + 1) % amountOfPhilosophers);
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    return nullptr;
+    while (true) {
+        std::cout << "Filozof numer: " << id << " myśli\n";
+
+        std::this_thread::sleep_for(std::chrono::seconds(8));
+
+        pthread_mutex_lock(&waitForFork[firstFork]);
+        std::cout << "Filozof " << id << " wzial widelec " << firstFork << "\n";
+        pthread_mutex_lock(&waitForFork[secondFork]);
+        std::cout << "Filozof " << id << " wzial widelec " << secondFork << " i zaczal jesc" << "\n";
+        std::this_thread::sleep_for(std::chrono::seconds(16));
+
+        pthread_mutex_unlock(&waitForFork[secondFork]);
+        std::cout << "Filozof " << id << " odlozyl widelec " << secondFork << "\n";
+        pthread_mutex_unlock(&waitForFork[firstFork]);
+        std::cout << "Filozof " << id << " odlozyl widelec " << firstFork << " i ponownie mysli" <<"\n";
+    }
 }
 
 int main(int argc, const char * argv[]) {
-    const int AMOUNT_OF_PHILOSOPHERS = std::stoi(argv[1]);
-    std::cout << "Liczba filozofów: " << AMOUNT_OF_PHILOSOPHERS << std::endl;
-    pthread_t philosophersThread[AMOUNT_OF_PHILOSOPHERS];
-    int ids[AMOUNT_OF_PHILOSOPHERS];
+    amountOfPhilosophers = std::stoi(argv[1]);
+    std::cout << "Liczba filozofów: " << amountOfPhilosophers << std::endl;
 
-    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
+    pthread_t philosophersThread[amountOfPhilosophers];
+    int ids[amountOfPhilosophers];
+    waitForFork = new pthread_mutex_t[amountOfPhilosophers];
+
+    for (int i = 0; i < amountOfPhilosophers; ++i) {
+        pthread_mutex_init(&waitForFork[i], nullptr);
+    }
+
+    for (int i = 0; i < amountOfPhilosophers; ++i) {
         ids[i] = i;
         pthread_create(&philosophersThread[i], nullptr, &philosopher, &ids[i]);
     }
 
-    waitForFork = new pthread_mutex_t[AMOUNT_OF_PHILOSOPHERS];
-    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
-        pthread_mutex_init(&waitForFork[i], nullptr);
-    }
-
-    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
+    for (int i = 0; i < amountOfPhilosophers; ++i) {
         pthread_join(philosophersThread[i], nullptr);
     }
 
-    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
+    for (int i = 0; i < amountOfPhilosophers; ++i) {
         pthread_mutex_destroy(&waitForFork[i]);
     }
 
+    delete[] waitForFork;
     return 0;
 }
