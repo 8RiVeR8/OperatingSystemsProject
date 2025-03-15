560075a67c12719a75b0714d58753e715c9d02a8
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-03-15 19:39:12 +0100

    add threads with philosophers

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,29 @@
 // Copyright 2025 Maks
+#include <pthread.h>
 #include <iostream>
-#include <cstdlib>
+#include <thread>
+
+void *test(void *arg) {
+    int id = *reinterpret_cast<int*>(arg);
+    std::cout << "Filozof numer: " << id << " myśli\n";
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    return nullptr;
+}
 
 int main(int argc, const char * argv[]) {
-    int amountOfPhilosophers = atoi(argv[1]);
-    std::cout << "Liczba filozofów: " << amountOfPhilosophers << std::endl;
+    const int AMOUNT_OF_PHILOSOPHERS = std::stoi(argv[1]);
+    std::cout << "Liczba filozofów: " << AMOUNT_OF_PHILOSOPHERS << std::endl;
+    pthread_t philosophersThread[AMOUNT_OF_PHILOSOPHERS];
+    int ids[AMOUNT_OF_PHILOSOPHERS];
+
+    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
+        ids[i] = i;
+        pthread_create(&philosophersThread[i], nullptr, &test, &ids[i]);
+    }
+
+    for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
+        pthread_join(philosophersThread[i], nullptr);
+    }
+
+    return 0;
 }
