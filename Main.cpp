7ef5831264373273f2fc38159a07b5df7e3a30a5
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-03-15 20:02:40 +0100

    add mutex for philosophers

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,13 @@
 #include <iostream>
 #include <thread>
 
+pthread_mutex_t waitForPrint = PTHREAD_MUTEX_INITIALIZER;
+
 void *test(void *arg) {
     int id = *reinterpret_cast<int*>(arg);
+    pthread_mutex_lock(&waitForPrint);
     std::cout << "Filozof numer: " << id << " myśli\n";
+    pthread_mutex_unlock(&waitForPrint);
     std::this_thread::sleep_for(std::chrono::seconds(3));
     return nullptr;
 }
@@ -24,6 +28,8 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; ++i) {
         pthread_join(philosophersThread[i], nullptr);
     }
+
+    pthread_mutex_destroy(&waitForPrint);
 
     return 0;
 }
