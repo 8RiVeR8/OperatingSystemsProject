a97c06d8b1251afb6d9fb3a881616c1e53bbcb8c
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-03-15 15:23:56 +0100

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2025 Maks
+#include <iostream>
+#include "Main.h"
+
+using std::cout;
+
+int main() {
+    cout << "Hello World!";
+}
---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Maksymilian Rzeczycki on 15/03/2025.
+//
+
+#ifndef OPERATINGSYSTEMSPROJECT_MAIN_H
+#define OPERATINGSYSTEMSPROJECT_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //OPERATINGSYSTEMSPROJECT_MAIN_H
